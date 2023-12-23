770ad1a23eb334dbb34f522ea93d790ddcd477a7
Edgar Pech <edgaropech@hotmail.com>
2023-12-22 20:15:00 -0500

    Add:_github pages

--- src/services/restaurants.js
+++ src/services/restaurants.js
@@ -1,5 +1,5 @@
 export default async function getAllRestaurants() {
-  const response = await fetch("/.netlify/functions/proxy");
+  const response = await fetch("https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json");
 
   if (!response.ok) {
     return new Error(`HTTP error! Status: ${response.status}`);
